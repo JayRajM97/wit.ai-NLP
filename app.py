50c07b1f749025d9753279bf16ab12d491d79f64
JAYRAJ MAKHAR <jayraj.mka@gmail.com>
2018-01-14 17:36:11 +0530

    webhook - resolve 3

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def verify():
     # WEBHOOK VERIFICATION
-    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub challenge"):
-        if not request.args.get("hub.verify_token") == "HELLO":
+    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+        if not request.args.get("hub.verify_token") == "secret":
             return "Verification Token Mismatch!", 403
         return request.args["hub.challenge"], 200
-    return "125577799", 200
+    return "Hey", 200
 
 
 
@@ -17,21 +17,3 @@ def verify():
 def hello_world():
     return 'Finally Done !'
 '''
-
-
-@app.route('/gandu')
-def gandu():
-    return """
-
-<html>
-  <head>
-    <title> Gandu Abhisex </title>
-  </head>
-  <body>
-    <h1> Agar tumne ye khola hai, to pakka gandu ho </h1>
-    <h4 style="bottom:0"> Made By Gandu Abhisex </h4>
-  </body>
-</html>
-
-
-    """
