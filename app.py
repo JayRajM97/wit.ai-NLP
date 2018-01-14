babda6bbef5be5d0a509f9d6667dbd2fc39fbfc6
JAYRAJ MAKHAR <jayraj.mka@gmail.com>
2018-01-14 17:16:41 +0530

    Setup Webhook 1

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
+def verify():
+    # WEBHOOK VERIFICATION
+    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub challenge"):
+        if not request.args.get("hub.verify_token") == "HELLO":
+            return "Verification Token Mismatch!", 403
+        return request.args["hub.challenge"], 200
+    return "All Set?", 200
+
+
+
+'''
 def hello_world():
     return 'Finally Done !'
+'''
+
 
 @app.route('/gandu')
 def gandu():
