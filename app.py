0275ec464969e5f77b823c6ce4d4e837fea5f39a
JAYRAJ MAKHAR <jayraj.mka@gmail.com>
2018-01-14 15:56:26 +0530

    App crashed bug - resolved and gandu added

--- app.py
+++ app.py
@@ -4,3 +4,20 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/gandu')
+def gandu():
+    return """
+
+<html>
+  <head>
+    <title> Gandu App </title>
+  </head>
+  <body>
+    <h1> Agar tumne ye khola hai, to pakka gandu ho </h1>
+    <h4 style="bottom:0"> Made By Gandu </h4>
+  </body>
+</html>
+
+
+    """
