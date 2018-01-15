6cee51df6a98003885c849fa90c946673b909b90
JAYRAJ MAKHAR <jayraj.mka@gmail.com>
2018-01-15 12:12:03 +0530

    Adding Echo Bot (To respond back the same message)

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 import os, sys
 from flask import Flask, request
+from pymessenger import Bot
+
+
 app = Flask(__name__)
+
+
+PAGE_ACCESS_TOKEN = "EAAbqd2mzriwBAHwhUk2pEYMwJm64s63uEjfXHMbPFwJPoUZAe9y4g7MK4KFMPXtDxUccbaHyOgxqXzOiZADvP7C0LbuQrVn91lKzO50tgcJiHZBrHyavAazgKZA1p4d618JuZAccdn3qT1qZA93QO1LV8FdDF5Xqr6ffqTLLq2QgZDZD"
+bot = Bot(PAGE_ACCESS_TOKEN)
 
 
 @app.route('/', methods=['GET'])
@@ -13,12 +20,32 @@ def verify():
     return "Hey", 200
 
 
-@app.route('/',methods=['POST'])
+@app.route('/', methods=['POST'])
 def webhook():
     data = request.get_json()
     log(data)
 
-    return "ALRIGHT!", 200
+    if data['object'] == 'page':
+        for entry in data['entry']:
+            for messaging_event in entry['messaging']:
+
+                # IDs
+                sender_id = messaging_event['sender']['id']
+                recipient_id = messaging_event['recipient']['id']
+
+                if messaging_event.get('message'):
+                    # Extracting text message
+                    if 'text' in messaging_event['message']:
+                        messaging_text = messaging_event['message']['text']
+                    else:
+                        messaging_text = 'no text'
+
+                    # Echo
+                    response = messaging_text
+                    bot.send_text_message(sender_id, response)
+
+    return "ALRIGHT!",    200
+
 
 def log(message):
     print(message)
